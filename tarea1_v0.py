9e09754fce93e54fc0410e5c3e08de1ac3fb2773
Felipe Valdebenito <felipe.vb1806@gmail.com>
2021-08-29 22:43:19 -0400

    tarea lista en version 1

--- tarea1_v0.py
+++ tarea1_v0.py
@@ -36,7 +36,8 @@ def crear_dama(x,y,r,g,b,radius):
                        y+numpy.sin(numpy.radians(angle+10))*radius, 
                        0.0, r, g, b])
     
-    return numpy.array(circle, dtype = numpy.float32)
+    ##return numpy.array(circle, dtype = numpy.float32)
+    return bs.Shape(circle, range(len(circle)))
 
 #########
 # 1. Crear funcion que genere la geometria del tablero y retorne un arreglo
@@ -136,9 +137,14 @@ def posicionar_en_tablero(lista1, lista2):
     # posicionar piezas rojas
     for i in range(len(lista1)):
         if i % 2 == 0:
-            crear_dama(lista1[i], lista[i+1], 1.0, 0.0, 0.0, radio)
+            piezas_tablero += [crear_dama(lista1[i], lista1[i+1], 1.0, 0.0, 0.0, radio)]
         i += 1
-
+    for j in range(len(lista2)):
+        if j % 2 == 0:
+            piezas_tablero += [crear_dama(lista2[j], lista2[j+1], 0.0, 0.0, 1.0, radio)]
+        j += 1    
+    #return bs.Shape(piezas_tablero, range(len(piezas_tablero)))
+    return piezas_tablero
 
 if __name__ == "__main__":
 
@@ -157,7 +163,7 @@ if __name__ == "__main__":
 
     glfw.make_context_current(window)
 
-    dama = crear_dama(0.5,0.0, 0.0, 1.0, 0.0, 0.2)
+    dama = crear_dama(-0.7, 0.7, 1.0, 0.0, 0.0, 0.05)
 
     # Defining shaders for our pipeline
     vertex_shader = """
@@ -222,6 +228,133 @@ if __name__ == "__main__":
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboTabla)
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, len(indices) * SIZE_IN_BYTES, indices, GL_STATIC_DRAW)
 
+    pipeline = es.SimpleShaderProgram()
+
+    piezas_rojas = ubicar_damas()[0]
+    piezas_azules = ubicar_damas()[1]
+
+    ##piezasShape = posicionar_en_tablero(piezas_rojas, piezas_azules)
+        ##piezasShape = dama
+    piezasShape0 = posicionar_en_tablero(piezas_rojas, piezas_azules)[0]
+    gpuPiezas0 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas0)
+    gpuPiezas0.fillBuffers(piezasShape0.vertices, piezasShape0.indices, GL_STATIC_DRAW)
+
+    piezasShape1 = posicionar_en_tablero(piezas_rojas, piezas_azules)[1]
+    gpuPiezas1 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas1)
+    gpuPiezas1.fillBuffers(piezasShape1.vertices, piezasShape1.indices, GL_STATIC_DRAW)
+
+    piezasShape2 = posicionar_en_tablero(piezas_rojas, piezas_azules)[2]
+    gpuPiezas2 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas2)
+    gpuPiezas2.fillBuffers(piezasShape2.vertices, piezasShape2.indices, GL_STATIC_DRAW)
+
+    piezasShape3 = posicionar_en_tablero(piezas_rojas, piezas_azules)[3]
+    gpuPiezas3 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas3)
+    gpuPiezas3.fillBuffers(piezasShape3.vertices, piezasShape3.indices, GL_STATIC_DRAW)
+
+    piezasShape4 = posicionar_en_tablero(piezas_rojas, piezas_azules)[4]
+    gpuPiezas4 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas4)
+    gpuPiezas4.fillBuffers(piezasShape4.vertices, piezasShape4.indices, GL_STATIC_DRAW)
+
+    piezasShape5 = posicionar_en_tablero(piezas_rojas, piezas_azules)[5]
+    gpuPiezas5 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas5)
+    gpuPiezas5.fillBuffers(piezasShape5.vertices, piezasShape5.indices, GL_STATIC_DRAW)
+
+    piezasShape6 = posicionar_en_tablero(piezas_rojas, piezas_azules)[6]
+    gpuPiezas6 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas6)
+    gpuPiezas6.fillBuffers(piezasShape6.vertices, piezasShape6.indices, GL_STATIC_DRAW)
+
+    piezasShape7 = posicionar_en_tablero(piezas_rojas, piezas_azules)[7]
+    gpuPiezas7 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas7)
+    gpuPiezas7.fillBuffers(piezasShape7.vertices, piezasShape7.indices, GL_STATIC_DRAW)
+
+    piezasShape8 = posicionar_en_tablero(piezas_rojas, piezas_azules)[8]
+    gpuPiezas8 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas8)
+    gpuPiezas8.fillBuffers(piezasShape8.vertices, piezasShape8.indices, GL_STATIC_DRAW)
+
+    piezasShape9 = posicionar_en_tablero(piezas_rojas, piezas_azules)[9]
+    gpuPiezas9 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas9)
+    gpuPiezas9.fillBuffers(piezasShape9.vertices, piezasShape9.indices, GL_STATIC_DRAW)
+
+    piezasShape10 = posicionar_en_tablero(piezas_rojas, piezas_azules)[10]
+    gpuPiezas10 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas10)
+    gpuPiezas10.fillBuffers(piezasShape10.vertices, piezasShape10.indices, GL_STATIC_DRAW)
+
+    piezasShape11 = posicionar_en_tablero(piezas_rojas, piezas_azules)[11]
+    gpuPiezas11 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas11)
+    gpuPiezas11.fillBuffers(piezasShape11.vertices, piezasShape11.indices, GL_STATIC_DRAW)
+
+    piezasShape12 = posicionar_en_tablero(piezas_rojas, piezas_azules)[12]
+    gpuPiezas12 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas12)
+    gpuPiezas12.fillBuffers(piezasShape12.vertices, piezasShape12.indices, GL_STATIC_DRAW)
+
+    piezasShape13 = posicionar_en_tablero(piezas_rojas, piezas_azules)[13]
+    gpuPiezas13 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas13)
+    gpuPiezas13.fillBuffers(piezasShape13.vertices, piezasShape13.indices, GL_STATIC_DRAW)
+
+    piezasShape14 = posicionar_en_tablero(piezas_rojas, piezas_azules)[14]
+    gpuPiezas14 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas14)
+    gpuPiezas14.fillBuffers(piezasShape14.vertices, piezasShape14.indices, GL_STATIC_DRAW)
+
+    piezasShape15 = posicionar_en_tablero(piezas_rojas, piezas_azules)[15]
+    gpuPiezas15 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas15)
+    gpuPiezas15.fillBuffers(piezasShape15.vertices, piezasShape15.indices, GL_STATIC_DRAW)
+
+    piezasShape16 = posicionar_en_tablero(piezas_rojas, piezas_azules)[16]
+    gpuPiezas16 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas16)
+    gpuPiezas16.fillBuffers(piezasShape16.vertices, piezasShape16.indices, GL_STATIC_DRAW)
+
+    piezasShape17 = posicionar_en_tablero(piezas_rojas, piezas_azules)[17]
+    gpuPiezas17 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas17)
+    gpuPiezas17.fillBuffers(piezasShape17.vertices, piezasShape17.indices, GL_STATIC_DRAW)
+
+    piezasShape18 = posicionar_en_tablero(piezas_rojas, piezas_azules)[18]
+    gpuPiezas18 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas18)
+    gpuPiezas18.fillBuffers(piezasShape18.vertices, piezasShape18.indices, GL_STATIC_DRAW)
+
+    piezasShape19 = posicionar_en_tablero(piezas_rojas, piezas_azules)[19]
+    gpuPiezas19 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas19)
+    gpuPiezas19.fillBuffers(piezasShape19.vertices, piezasShape19.indices, GL_STATIC_DRAW)
+
+    piezasShape20 = posicionar_en_tablero(piezas_rojas, piezas_azules)[20]
+    gpuPiezas20 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas20)
+    gpuPiezas20.fillBuffers(piezasShape20.vertices, piezasShape20.indices, GL_STATIC_DRAW)
+
+    piezasShape21 = posicionar_en_tablero(piezas_rojas, piezas_azules)[21]
+    gpuPiezas21 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas21)
+    gpuPiezas21.fillBuffers(piezasShape21.vertices, piezasShape21.indices, GL_STATIC_DRAW)
+
+    piezasShape22 = posicionar_en_tablero(piezas_rojas, piezas_azules)[22]
+    gpuPiezas22 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas22)
+    gpuPiezas22.fillBuffers(piezasShape22.vertices, piezasShape22.indices, GL_STATIC_DRAW)
+
+    piezasShape23 = posicionar_en_tablero(piezas_rojas, piezas_azules)[23]
+    gpuPiezas23 = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuPiezas23)
+    gpuPiezas23.fillBuffers(piezasShape23.vertices, piezasShape23.indices, GL_STATIC_DRAW)
+
 #############    
     #vboDama = glGenBuffers(1)
     
@@ -284,6 +417,32 @@ if __name__ == "__main__":
         glBindVertexArray(vaoTabla)
         glDrawElements(GL_TRIANGLES, size, GL_UNSIGNED_INT, None)
 
+
+        pipeline.drawCall(gpuPiezas0)
+        pipeline.drawCall(gpuPiezas1)
+        pipeline.drawCall(gpuPiezas2)
+        pipeline.drawCall(gpuPiezas3)
+        pipeline.drawCall(gpuPiezas4)
+        pipeline.drawCall(gpuPiezas5)
+        pipeline.drawCall(gpuPiezas6)
+        pipeline.drawCall(gpuPiezas7)
+        pipeline.drawCall(gpuPiezas8)
+        pipeline.drawCall(gpuPiezas9)
+        pipeline.drawCall(gpuPiezas10)
+        pipeline.drawCall(gpuPiezas11)
+        pipeline.drawCall(gpuPiezas12)
+        pipeline.drawCall(gpuPiezas13)
+        pipeline.drawCall(gpuPiezas14)
+        pipeline.drawCall(gpuPiezas15)
+        pipeline.drawCall(gpuPiezas16)
+        pipeline.drawCall(gpuPiezas17)
+        pipeline.drawCall(gpuPiezas18)
+        pipeline.drawCall(gpuPiezas19)
+        pipeline.drawCall(gpuPiezas20)
+        pipeline.drawCall(gpuPiezas21)
+        pipeline.drawCall(gpuPiezas22)
+        pipeline.drawCall(gpuPiezas23)
+
         glBindVertexArray(0)
 
         #glBindVertexArray(VAO)
@@ -296,5 +455,29 @@ if __name__ == "__main__":
     glDeleteBuffers(1, [eboTabla])
     glDeleteBuffers(1, [vboTabla])
     glDeleteVertexArrays(1, [vaoTabla])
+    gpuPiezas0.clear()
+    gpuPiezas1.clear()
+    gpuPiezas2.clear()
+    gpuPiezas3.clear()
+    gpuPiezas4.clear()
+    gpuPiezas5.clear()
+    gpuPiezas6.clear()
+    gpuPiezas7.clear()
+    gpuPiezas8.clear()
+    gpuPiezas9.clear()
+    gpuPiezas10.clear()
+    gpuPiezas11.clear()
+    gpuPiezas12.clear()
+    gpuPiezas13.clear()
+    gpuPiezas14.clear()
+    gpuPiezas15.clear()
+    gpuPiezas16.clear()
+    gpuPiezas17.clear()
+    gpuPiezas18.clear()
+    gpuPiezas19.clear()
+    gpuPiezas20.clear()
+    gpuPiezas21.clear()
+    gpuPiezas22.clear()
+    gpuPiezas23.clear()
 
     glfw.terminate()
